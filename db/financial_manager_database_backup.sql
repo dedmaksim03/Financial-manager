0aaa48282b2f30ad5a43065b37bf8a6c33b37315
gorskiyms <gorskiyms@rbdata.group>
2025-06-12 15:40:15 +0300

    Обновлен бэкап бд

--- db/financial_manager_database_backup.sql
+++ db/financial_manager_database_backup.sql
@@ -1,9 +1,105 @@
 --
+-- PostgreSQL database cluster dump
+--
+
+-- Started on 2025-06-12 15:36:34
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+CREATE ROLE postgres;
+ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS;
+
+--
+-- User Configurations
+--
+
+
+
+
+
+
+
+
+--
+-- Databases
+--
+
+--
+-- Database "template1" dump
+--
+
+\connect template1
+
+--
 -- PostgreSQL database dump
 --
 
 -- Dumped from database version 17.4 (Debian 17.4-1.pgdg120+2)
--- Dumped by pg_dump version 17.4 (Debian 17.4-1.pgdg120+2)
+-- Dumped by pg_dump version 17.0
+
+-- Started on 2025-06-12 15:36:34
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2025-06-12 15:36:35
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "financial_manager_database" dump
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.4 (Debian 17.4-1.pgdg120+2)
+-- Dumped by pg_dump version 17.0
+
+-- Started on 2025-06-12 15:36:35
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 3387 (class 1262 OID 16384)
+-- Name: financial_manager_database; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE financial_manager_database WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE financial_manager_database OWNER TO postgres;
+
+\connect financial_manager_database
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -22,6 +118,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- TOC entry 217 (class 1259 OID 16385)
 -- Name: actions; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -38,6 +135,7 @@ CREATE TABLE public.actions (
 ALTER TABLE public.actions OWNER TO postgres;
 
 --
+-- TOC entry 218 (class 1259 OID 16390)
 -- Name: actions_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -52,6 +150,8 @@ CREATE SEQUENCE public.actions_id_seq
 ALTER SEQUENCE public.actions_id_seq OWNER TO postgres;
 
 --
+-- TOC entry 3388 (class 0 OID 0)
+-- Dependencies: 218
 -- Name: actions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -59,6 +159,7 @@ ALTER SEQUENCE public.actions_id_seq OWNED BY public.actions.id;
 
 
 --
+-- TOC entry 219 (class 1259 OID 16391)
 -- Name: categories; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -73,6 +174,7 @@ CREATE TABLE public.categories (
 ALTER TABLE public.categories OWNER TO postgres;
 
 --
+-- TOC entry 220 (class 1259 OID 16396)
 -- Name: categories_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -87,6 +189,8 @@ CREATE SEQUENCE public.categories_id_seq
 ALTER SEQUENCE public.categories_id_seq OWNER TO postgres;
 
 --
+-- TOC entry 3389 (class 0 OID 0)
+-- Dependencies: 220
 -- Name: categories_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -94,6 +198,7 @@ ALTER SEQUENCE public.categories_id_seq OWNED BY public.categories.id;
 
 
 --
+-- TOC entry 221 (class 1259 OID 16397)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -102,13 +207,14 @@ CREATE TABLE public.users (
     username character varying NOT NULL,
     password character varying NOT NULL,
     refresh_token character varying,
-    expires timestamp
+    expires timestamp without time zone
 );
 
 
 ALTER TABLE public.users OWNER TO postgres;
 
 --
+-- TOC entry 222 (class 1259 OID 16402)
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -123,6 +229,8 @@ CREATE SEQUENCE public.users_id_seq
 ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
 
 --
+-- TOC entry 3390 (class 0 OID 0)
+-- Dependencies: 222
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -130,6 +238,7 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- TOC entry 3220 (class 2604 OID 16403)
 -- Name: actions id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -137,6 +246,7 @@ ALTER TABLE ONLY public.actions ALTER COLUMN id SET DEFAULT nextval('public.acti
 
 
 --
+-- TOC entry 3221 (class 2604 OID 16404)
 -- Name: categories id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -144,6 +254,7 @@ ALTER TABLE ONLY public.categories ALTER COLUMN id SET DEFAULT nextval('public.c
 
 
 --
+-- TOC entry 3222 (class 2604 OID 16405)
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -151,6 +262,8 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
+-- TOC entry 3376 (class 0 OID 16385)
+-- Dependencies: 217
 -- Data for Name: actions; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -159,6 +272,8 @@ COPY public.actions (id, name, message, sum, date, category_id) FROM stdin;
 
 
 --
+-- TOC entry 3378 (class 0 OID 16391)
+-- Dependencies: 219
 -- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -167,14 +282,22 @@ COPY public.categories (id, name, color, user_id) FROM stdin;
 
 
 --
+-- TOC entry 3380 (class 0 OID 16397)
+-- Dependencies: 221
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.users (id, username, password, refresh_token) FROM stdin;
+COPY public.users (id, username, password, refresh_token, expires) FROM stdin;
+4	ded_maksim	$2b$10$/aDBwHfwRRAIJrE07FTNXOJrjk1hGDi.kJXxFLrwdTp80R9cbTSz2		2025-05-14 22:48:08.426
+5	kate	$2b$10$3CT3clDPsOF0rJz/r3KKpe5hl10bbIpM29p4lTTrgHAbxOZ8mFxuC		2025-05-14 22:48:48.58
+3	ded	$2b$10$THPUrEN3lVSkowaaxaBxSujcGS3eIGrzMqneZ9T3wtvBm3F.EzhYa	1c2fddbc-1dd1-4129-a096-7e5f933c9346	2025-06-12 20:38:23.511
+2	admin	$2b$10$taUXhz0QGiQKW6GJQbFrc.4.wYmJ9hJjPPvwH39oNcuN.wswtO7ky	61f7c739-2760-4e26-92aa-8e09d5b5aa7b	2025-06-26 15:03:08.764
 \.
 
 
 --
+-- TOC entry 3391 (class 0 OID 0)
+-- Dependencies: 218
 -- Name: actions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -182,20 +305,25 @@ SELECT pg_catalog.setval('public.actions_id_seq', 1, false);
 
 
 --
+-- TOC entry 3392 (class 0 OID 0)
+-- Dependencies: 220
 -- Name: categories_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.categories_id_seq', 1, false);
+SELECT pg_catalog.setval('public.categories_id_seq', 1, true);
 
 
 --
+-- TOC entry 3393 (class 0 OID 0)
+-- Dependencies: 222
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_id_seq', 5, true);
 
 
 --
+-- TOC entry 3224 (class 2606 OID 16407)
 -- Name: actions actions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -204,6 +332,7 @@ ALTER TABLE ONLY public.actions
 
 
 --
+-- TOC entry 3226 (class 2606 OID 16409)
 -- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -212,6 +341,7 @@ ALTER TABLE ONLY public.categories
 
 
 --
+-- TOC entry 3228 (class 2606 OID 16411)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -220,6 +350,7 @@ ALTER TABLE ONLY public.users
 
 
 --
+-- TOC entry 3229 (class 2606 OID 16412)
 -- Name: actions actions_category_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -228,6 +359,7 @@ ALTER TABLE ONLY public.actions
 
 
 --
+-- TOC entry 3230 (class 2606 OID 16417)
 -- Name: categories categories_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -235,7 +367,15 @@ ALTER TABLE ONLY public.categories
     ADD CONSTRAINT categories_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
 
 
+-- Completed on 2025-06-12 15:36:35
+
 --
 -- PostgreSQL database dump complete
+--
+
+-- Completed on 2025-06-12 15:36:35
+
+--
+-- PostgreSQL database cluster dump complete
 --
 
