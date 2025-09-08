4f04a6bd5f162aa852027c59013e04af88ae932f
gorskiyms <gorskiyms@rbdata.group>
2025-09-08 12:00:34 +0300

    Сделать цвет неиспользуемых категорий более похожим на их истинный цвет

--- frontend/src/components/categoriesPalette/index.tsx
+++ frontend/src/components/categoriesPalette/index.tsx
@@ -15,56 +15,38 @@ export const CategoriesPalette: React.FC<Props> = ({
   setSelectedCategoryId,
 }) => {
 
-  if (!setSelectedCategoryId)
-    return (
-      <div className={styles.palette}>
-        {categories.map((category) => {
-          const categorySum = category.sum || 0;
-          return (
-            <div
-              key={category.id}
-              className={`${styles.colorItem}`}
-              style={{ 
-                backgroundColor: category.sum != 0 ? category.color : "#dedede"
-              }}
-              title={category.name}
-            >
-              <div className={styles.categoryInfo}>
-                <div className={styles.categoryName}>{category.name}</div>
-                <div className={styles.categoryAmount}>
-                  {categorySum.toLocaleString('ru-RU', { style: 'currency', currency: 'RUB' })}
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>  
-  )
+  function hexToRGBA(hex: string, alpha = 0.3) {
+    const r = parseInt(hex.slice(1, 3), 16)
+    const g = parseInt(hex.slice(3, 5), 16)
+    const b = parseInt(hex.slice(5, 7), 16)
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`
+  }
 
   return (
-      <div className={styles.palette}>
+    <div className={styles.palette}>
         {categories.map((category) => {
           const categorySum = category.sum || 0;
           return (
             <div
-              key={category.id}
-              className={`${styles.colorItem} ${selectedCategoryId === category.id ? styles.selected : ''}`}
-              style={{ 
-                backgroundColor: category.sum != 0 ? category.color : "#dedede"
-              }}
-              onClick={() => setSelectedCategoryId(category.id)}
-              title={category.name}
+              key={category.id} 
+              className={setSelectedCategoryId ?`${styles.colorItem} ${selectedCategoryId === category.id ? styles.selected : ''}` : `${styles.colorItem}`}
+              style={category.sum == 0 ? { 
+                backgroundColor: category.color,
+                opacity: 0.3
+              }: {backgroundColor: category.color}}
+              onClick={setSelectedCategoryId ? () => setSelectedCategoryId(category.id) : () => {}}
+              title={category.name}    
             >
-              {selectedCategoryId === category.id && <span className={styles.checkmark}>✓</span>}
+              {(selectedCategoryId === category.id && setSelectedCategoryId) && <span className={styles.checkmark}>✓</span>}     
               <div className={styles.categoryInfo}>
                 <div className={styles.categoryName}>{category.name}</div>
                 <div className={styles.categoryAmount}>
                   {categorySum.toLocaleString('ru-RU', { style: 'currency', currency: 'RUB' })}
                 </div>
-              </div>
-            </div>
-          );
+              </div>  
+            </div>   
+            );
         })}
-      </div>
-  );
+    </div> 
+  )
 };
